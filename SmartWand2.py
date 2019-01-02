702242551ece8d7023c1db44df1177ed1f2ac705
maspieljr <maspieljunk@gmail.com>
2019-01-01 19:13:18 -0600

    Update SmartWand2.py

--- SmartWand2.py
+++ SmartWand2.py
@@ -1,3 +1,14 @@
+# /etc/init.d/sample.py
+### BEGIN INIT INFO
+# Provides:          sample.py
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start daemon at boot time
+# Description:       Enable service provided by daemon.
+### END INIT INFO
+
 #Wand Module Imports
 from kano_wand.kano_wand import Shop, Wand, PATTERN
 import moosegesture as mg
